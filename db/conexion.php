bf6b34241dd3169dd9abf1ca8d2b6501d1df9290
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-05-19 11:38:03 -0600

    Agregar librerias

---
+++ db/conexion.php
@@ -0,0 +1,10 @@
+<?php
+    require 'parametros.php';
+    class Conexion {
+        protected $con;
+        function __construct() {
+            $this->con=new mysqli(SERVER,USER,PASSWORD,BASE);
+            $this->con->set_charset(CHAR);
+        }
+    }
+?>
---
+++ db/parametros.php
@@ -0,0 +1,7 @@
+<?php
+    define("SERVER", "localhost");
+    define("USER", "root");
+    define("PASSWORD","");
+    define("BASE","esmpresa2");
+    define("CHAR","utf-8");
+?>
