a7dbc68967944ac4221b5a93a63889e00408a1c4
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-05-20 23:50:30 -0600

    Agregar Login Vista

---
+++ views/loginCli.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" src="sweetalert2.all.min.js"></script>
+    <title>Ventana de Logeo</title>
+</head>
+<body>
+    <header>
+        <h1>Login</h1>
+    </header>
+    <section> <center>
+        <div class="column col-md-4">
+            <form action="#" >
+                login <input type="text" name="login" class="form-control">
+                Password <input type="password" name="password"  class="form-control">
+                <input type="submit" name="validar" class="btn btn-success">
+            </form>
+        </div>
+    </section></center>
+    <footer></footer>
+    
+</body>
+</html>
