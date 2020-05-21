7c946906b7936d2e8cdea12d4c0076bb3bb95be5
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-05-20 23:49:30 -0600

    Agregar control Empleado

---
+++ controllers/controlEmpleado.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    if(isset($_REQUEST["c"])) {
+        session_destroy();
+        header("Location:controlUserCli.php");
+    }
+    
+    if(isset($_SESSION["s1"])) {
+        echo "Bienvenida/o".$_SESSION["s1"];
+        echo " <a href='controlEmpleado.php?c=1'>Cerrar Sesión</a>";
+    } else {
+        header("Location:controlUserCli.php");
+    }
+
+?>
