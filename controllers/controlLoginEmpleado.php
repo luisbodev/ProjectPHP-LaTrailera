20b94caff5f34b1bfdcf1702262aa86f354729d9
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-05-25 07:38:07 -0600

    agregar funcion tomar rol

--- controllers/controlLoginEmpleado.php
+++ controllers/controlLoginEmpleado.php
@@ -6,14 +6,20 @@
 	
 	// Validar la entrada el empleado solo sucederá si el usuario comienza con "emp"
 	if(isset($_REQUEST["btnValidarEmpleado"])) {
-	    $r=$obUser->validarUsuarioEmpleado($_REQUEST["usuarioCli"], $_REQUEST["passwordEmp"]);
-	    if($r==1) {
+		$r=$obUser->validarUsuarioEmpleado($_REQUEST["usuarioCli"], $_REQUEST["passwordEmp"]);
+		//$rol=$obUser->tomarRol($_REQUEST["usuarioCli"], $_REQUEST["passwordEmp"]);
+		$rol=$obUser->tomarRol($_REQUEST["usuarioCli"], $_REQUEST["passwordEmp"]);
+	    if($r==1 && $rol==0) {
 	        session_start();
 	        $_SESSION["s1"]=$_REQUEST["usuarioCli"];//Identificador de la seción
-	        header("Location:controlEmpleado.php");
-	    }
-	    else
-	        echo "<script>alert('Usuario o contraseña no validos')</script>";
+			//header("Location:controlEmpleado.php");
+			echo "<script>alert('Empleado común')</script>";
+	    } elseif($r==1 && $rol==1) {
+			echo "<script>alert('Empleado Admin')</script>"; /*Aquí irá el código para abrir la ventana de admonistrador*/
+		} else {
+			echo "<script>alert('Usuario o contraseña no validos')</script>";
+		}
+	        
 	}
 
 	require "../views/loginEmpleado.php";
