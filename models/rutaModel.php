4c635555aa1209eee0f73e5b4fd97307eb53cb0b
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-06-04 23:33:17 -0600

    Agregar cambio de ruta, vehiculo y carga

--- models/rutaModel.php
+++ models/rutaModel.php
@@ -1,6 +1,8 @@
 <?php  
 	require '../db/conexion.php';
 	require 'ruta.php';
+	require 'vehiculo.php';
+	require 'carga.php';
 
 	class RutaModel extends Conexion
 	{
@@ -14,11 +16,13 @@
 			$res=$this->con->query("select * from ruta");
 			$r=array();
 			while($row=$res->fetch_assoc()){
-				$e=new Ruta($row["idRuta"],$row["kilometraje"],$row["latPuntoA"],$row["lngPuntoA"],$row["latPuntoB"],$row["lngPuntoB"],$row["idMotorista"]);
+				$e=new Ruta($row["idRuta"],$row["kilometraje"],$row["latPuntoA"],$row["lngPuntoA"],$row["latPuntoB"],$row["lngPuntoB"],$row["idMotorista"],$row["idVehiculo"],$row["idCarga"]);
 				$r[]=$e;
 			}
 			return $r;
 		}
+
+
 		function getMotorista(){
 			$res=$this->con->query("select * from motorista");
 			$r=array();
@@ -28,10 +32,32 @@
 		return $r;
 		}
 
+		function getVehiculo(){
+			$res=$this->con->query("select * from vehiculo");
+			$r=array();
+			while($row=$res->fetch_assoc()){
+				$v=new Vehiculo($row["idVehiculo"],$row["marca"],$row["placa"],$row["modelo"],$row["tazaCombustible"],$row["capacidadCombustible"],$row["kmRecorridos"]);
+				$r[]=$v;
+			}
+			return $r;
+		}
+
+		function getCarga(){
+			$res=$this->con->query("select * from carga");
+			$r=array();
+			while($row=$res->fetch_assoc()){
+				$c=new Carga($row["idCarga"],$row["descripcion"],$row["peso"]);
+				$r[]=$c;
+			}
+			return $r;
+		}
+
+
+
 		function insertarRuta($e){
 			try{
-				$para=$this->con->prepare("insert into ruta(kilometraje,latPuntoA,lngPuntoA,latPuntoB,lngPuntoB,idMotorista) values(?,?,?,?,?,?)");
-				$para->bind_param('ssssss',$b,$c,$d,$f,$g,$h);
+				$para=$this->con->prepare("insert into ruta(kilometraje,latPuntoA,lngPuntoA,latPuntoB,lngPuntoB,idMotorista, idVehiculo, idCarga) values(?,?,?,?,?,?,?,?)");
+				$para->bind_param('ssssssss',$b,$c,$d,$f,$g,$h,$i,$j);
 				
 				$b=$e->getKilometraje();
 				$c=$e->getLatPuntoA();
@@ -39,6 +65,8 @@
 				$f=$e->getLatPuntoB();
 				$g=$e->getLngPuntoB();
 				$h=$e->getIdMotorista();
+				$i=$e->getIdVehiculo();
+				$j=$e->getIdCarga();
 				$para->execute();
 			}catch(Exception $ex){
 				return $ex;
@@ -49,8 +77,8 @@
 
 		function modificarRuta($e){
 			try{
-				$para=$this->con->prepare("update ruta set kilometraje=?,latPuntoA=?,lngPuntoA=?,latPuntoB=?,lngPuntoB=?,idMotorista=? where idRuta=?");
-				$para->bind_param('sssssss',$a,$b,$c,$d,$f,$g,$h);
+				$para=$this->con->prepare("update ruta set kilometraje=?,latPuntoA=?,lngPuntoA=?,latPuntoB=?,lngPuntoB=?,idMotorista=?, idVehiculo=?, idCarga=? where idRuta=?");
+				$para->bind_param('sssssssss',$a,$b,$c,$d,$f,$g,$i,$j,$h);
 				
 				$a=$e->getKilometraje();
 				$b=$e->getLatPuntoA();
@@ -58,7 +86,10 @@
 				$d=$e->getLatPuntoB();
 				$f=$e->getLngPuntoB();
 				$g=$e->getIdMotorista();
+				$i=$e->getIdVehiculo();
+				$j=$e->getIdCarga();
 				$h=$e->getIdRuta();
+				
 				
 				$para->execute();
 			}catch(Exception $ex){
