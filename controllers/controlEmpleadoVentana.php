fee844cf577677dafeb393eaed7c0e742c7f35e9
Luis Bonilla <luisbonilla345@gmail.com>
2020-06-15 16:31:04 -0600

    menu Empleado

--- controllers/controlEmpleadoVentana.php
+++ controllers/controlEmpleadoVentana.php
@@ -1,3 +1,15 @@
 <?php 
+    session_start();
+    if(isset($_REQUEST["c"])) {
+        session_destroy();
+        header("Location:controlLogin.php");
+    }
+    
+    if(isset($_SESSION["empleado"])) {
+        //echo "Bienvenida/o ".$_SESSION["cliente"];
+        //echo " <a href='controlEmpleado.php?c=1'>Cerrar Sesión</a>";
+    } else {
+        header("Location:controlLogin.php");
+    }
     require "../views/empleadoVentanaVista.php";
 ?>
