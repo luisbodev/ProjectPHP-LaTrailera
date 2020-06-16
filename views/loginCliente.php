8cfeeb4cd125887002e8e9b91190d4c5b177abd0
Luis Bonilla <luisbonilla345@gmail.com>
2020-06-16 00:17:11 -0600

    Cambio en titulo de pagina

--- views/loginCliente.php
+++ views/loginCliente.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar Sesión - La Trailera</title>
+    <title>Iniciar Sesión Cliente - La Trailera</title>
     <!--Dependencias-->
     <!-- <link rel="stylesheet" type="text/css" href="dependencias/bootstrap/css/bootstrap.css"> -->
     <script type="text/javascript" src="dependencias/jquery.js"></script>
