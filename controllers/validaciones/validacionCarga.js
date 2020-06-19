2ffcba9d18930a42f298744d79774ca486f28b7c
Ismael Castillo <ismael.castillo19@itca.edu.sv>
2020-06-18 20:38:38 -0600

    carga

--- controllers/validaciones/validacionCarga.js
+++ controllers/validaciones/validacionCarga.js
@@ -1,21 +1,21 @@
-function validar2(){
-	var descripcion, peso;
+function validar2() {
+    var descripcion, peso;
 
-	descripcion=document.getElementById("descripcion").value;
-	peso=document.getElementById("peso").value;
-	
-	
+    descripcion = document.getElementById("descripcion").value;
+    peso = document.getElementById("peso").value;
 
-	if (descripcion.length>50 || descripcion.length<4) {
-		Swal.fire({
-           title: "Descripcion Demasiado Larga"
+
+
+    if (descripcion.length > 50 || descripcion.length < 4) {
+        Swal.fire({
+            title: "Descripcion Demasiado Larga"
         });
-		return false;
-	}else if(peso.length>15) || peso.length<4{
-		Swal.fire({
-           title: "Peso fuera de rango"
+        return false;
+    } else if (peso.length > 15 || peso.length < 4) {
+        Swal.fire({
+            title: "Peso fuera de rango"
         });
-		return false;
-	}
-	
+        return false;
+    }
+
 }
